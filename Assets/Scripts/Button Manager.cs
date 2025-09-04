1bd52077a416298df5d3478004334c118d89e01d
treetreet <treetreet@kakao.com>
2025-09-04 22:17:52 +0900

    Initial commit

---
+++ Assets/Scripts/Button Manager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManager : MonoBehaviour
+{
+    [SerializeField] private GameObject buttonSet;
+    [SerializeField] private Button[] buttons;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
